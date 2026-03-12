920ff53f4c46da89992e113e41eda4227826adba
DanaZaid5 <danaalsalami2003@gmail.com>
2026-03-12 05:03:52 +0300

    Update build.gradle.kts

--- lexia/android/build.gradle.kts
+++ lexia/android/build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
-  // ...
-
-  // Add the dependency for the Google services Gradle plugin
-  id("com.google.gms.google-services") version "4.4.4" apply false
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 
